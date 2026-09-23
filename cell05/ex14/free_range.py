6b073c69b39a41adbeefe647ccd8290d96e052c4
stamp <67070249@kmitl.ac.th>
2026-09-23 19:18:42 +0700

    Update free_range.py

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -5,5 +5,5 @@ import sys
 if len(sys.argv) != 3:
     print("none")
 else:
-    array = list(range(int(sys.argv[1]), int(sys.argv[2])))
+    array = list(range(int(sys.argv[1]), int(sys.argv[2]) + 1))
     print(array)
